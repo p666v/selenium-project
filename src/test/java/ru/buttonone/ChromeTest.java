aa1a78954b71fd824f58e85dfda8facefaa4814d
Konovalov Pavel <108332615+p666v@users.noreply.github.com>
2024-01-25 22:56:47 +0300

    Добавил зависимость TestNG, тесты, и Properties (#4)
    * Добавил зависимость TestNG, тесты, и Properties
    
    * Перенёс проперти в java
    
    * Поправил название пакета, добавил выбор драйвера через properties
    
    * Delete src/main/java/ru/itsjava directory
    
    * Заменил устаревший метод
    
    ---------
    
    Co-authored-by: Pavel <p666v@yandex.ru>

---
+++ src/test/java/ru/buttonone/ChromeTest.java
@@ -0,0 +1,37 @@
+package ru.buttonone;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class ChromeTest {
+    private WebDriver driver;
+    private final ConfProperties confProperties = new ConfProperties();
+
+    @BeforeClass
+    public void setupClass() {
+        driver = WebDriverManager.getInstance(confProperties.getProperty("browserName")).create();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        System.out.println("The profile setup process is completed");
+    }
+
+    @Test
+    public void testPageTitle() {
+        driver.get(confProperties.getProperty("test_site"));
+        assertEquals(driver.getTitle(), "Добро пожаловать в Steam");
+    }
+
+    @AfterClass
+    void setupTest() {
+        driver.quit();
+    }
+
+}
